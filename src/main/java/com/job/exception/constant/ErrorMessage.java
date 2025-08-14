0f03e0d937589e8ec9cb413a3f3cbe5bbfb95b20
nijat <khanalievn@gmail.com>
2025-08-14 17:52:29 +0400

    feat: ErrorMessage constants created

---
+++ src/main/java/com/job/exception/constant/ErrorMessage.java
@@ -0,0 +1,7 @@
+package com.job.exception.constant;
+
+public final class ErrorMessage {
+    public static final String NOT_FOUND_MESSAGE = "%s not found";
+    public static final String INVALID_INPUT_MESSAGE = "Invalid %s";
+    public static final String ALREADY_EXIST_MESSAGE = "%s already exist";
+}
