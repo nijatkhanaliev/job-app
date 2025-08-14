3d59750d3ca1f95267cbd7ec4245f93ee973f68d
nijat <khanalievn@gmail.com>
2025-08-14 17:51:59 +0400

    feat: errorCode constant created

---
+++ src/main/java/com/job/exception/constant/ErrorCode.java
@@ -0,0 +1,7 @@
+package com.job.exception.constant;
+
+public final class ErrorCode {
+    public static final String NOT_FOUND = "NOT_FOUND";
+    public static final String INVALID_INPUT = "INVALID_INPUT";
+    public static final String ALREADY_EXIST = "ALREADY_EXIST";
+}
